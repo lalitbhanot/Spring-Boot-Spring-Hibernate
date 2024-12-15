cbf9ce0a2c1fe8ae4be59111173684edcfd6f862
lalit <lalitthebest@gmail.com>
2024-12-15 16:32:14 +0100

    1. made changes in pom.xml to add dependencies. 2.added mysql details application.properties. 3. added commandlinerunner in application 4. added new entities for mapping .

--- src/main/java/com/example/lalit/LalitApplication.java
+++ src/main/java/com/example/lalit/LalitApplication.java
@@ -1,7 +1,9 @@
 package com.example.lalit;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class LalitApplication {
@@ -10,4 +12,12 @@ public class LalitApplication {
 		SpringApplication.run(LalitApplication.class, args);
 	}
 
+
+@Bean
+public CommandLineRunner commandLineRunner (String[] args)
+{
+	return  runner ->{
+		System.out.println("Hello World");
+	};
+}
 }
