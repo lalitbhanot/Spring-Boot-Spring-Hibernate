84a24ff4177ea20083bb7fa2f0e51d2418ccc87b
lalitbhanot <76906227+lalitbhanot@users.noreply.github.com>
2024-12-15 13:39:28 +0100

    Add files via upload

---
+++ src/test/java/com/example/lalit/LalitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lalit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LalitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
